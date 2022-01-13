c5e20623ee2d3f05cfc2072daeb3ad4d6f103baf
brasiliancode <studioproend@gmail.com>
2022-01-13 14:43:20 -0300

    version1.0

---
+++ autenticacao/urls.py
@@ -0,0 +1,9 @@
+from django import views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('logar/', views.logar, name='logar')
+]
+
