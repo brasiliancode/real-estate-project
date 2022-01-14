41e1ddb46eef15b2e288def5745f18f578b18a16
brasiliancode <studioproend@gmail.com>
2022-01-14 12:29:32 -0300

    v1.0

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,12 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.contrib import auth
+
+
 
 def cadastro(request):
     if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('/')
         return render(request, 'cadastro.html')
     elif request.method == 'POST':
-        return HttpResponse('Teste')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        if len(username.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/auth/cadastro')
+        
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuario ja existe')
+            return redirect('/auth/cadastro')
+        
+        try:
+            user = User.objects.create_user(username=username, email=email, password=senha)
+            user.save()
+            messages.add_message(request, constants.SUCCESS, 'Usuario cadastrado com sucesso')
+            return redirect('/auth/logar')
+        except:
+            messages.add_message(request, constants.ERROR, 'Error interno do sistema')
+            return redirect('/auth/logar')
+
 
 
 def logar(request):
-   return render(request, 'logar.html')
+    if request.method =='GET':
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request,'logar.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        usuario = auth.authenticate(username=username, password=senha)
+
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Usuario ou senha invalidos')
+            return redirect('/auth/logar')
+        else:
+            auth.login(request, usuario)
+            return redirect('/')
+    
+
+
+        
+        
+
