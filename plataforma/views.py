f87498a29551086b86742e028ac9556b2d9a28b5
brasiliancode <studioproend@gmail.com>
2022-01-15 10:49:33 -0300

    v3

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,8 +1,8 @@
 import re
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Imovel, Cidade
+from .models import Imovel, Cidade, Visitas
 from django.shortcuts import get_object_or_404
 
 @login_required(login_url='/auth/logar/')
@@ -35,3 +35,29 @@ def imovel(request, id):
     imovel = get_object_or_404(Imovel, id=id)
     sugestoes = Imovel.objects.filter(cidade=imovel.cidade).exclude(id=id)[:2]
     return render(request, 'imovel.html', {'imovel': imovel, 'sugestoes': sugestoes, 'id':id})
+
+def agendar_visitas(request):
+    usuario = request.user
+    dia = request.POST.get('dia')
+    horario = request.POST.get('horario')
+    id_imovel = request.POST.get('id_imovel')  
+
+    visitas = Visitas(
+        imovel_id = id_imovel,
+        usuario = usuario,
+        dia = dia,
+        horario = horario
+    )
+    visitas.save()
+
+    return redirect('/agendamentos')
+
+def agendamentos(request):
+    visitas = Visitas.objects.filter(usuario=request.user)
+    return render(request, 'agendamentos.html', {'visitas': visitas})
+
+def cancelar_agendamento(request, id):
+    visitas = get_object_or_404(Visitas, id=id)
+    visitas.status = 'C'
+    visitas.save()
+    return redirect('/agendamentos')
